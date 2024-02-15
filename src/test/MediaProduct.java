046a2625977f1860aefcb670e3ab5511203d95db
Micheal Whaley <michealwhaley42@gmail.com>
2024-02-15 13:07:36 -0600

    added getters/setters to mediaproduct

--- src/test/MediaProduct.java
+++ src/test/MediaProduct.java
@@ -23,4 +23,33 @@ public class MediaProduct {
         this.year = otherMediaProduct.year;
         this.genre = otherMediaProduct.genre;
     }
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+    public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+    public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+    public Genre getGenre() {
+		return genre;
+	}
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
 }
