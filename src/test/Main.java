fb05af12d222ed2163109d22df8a5a9f82e8e9f7
Micheal Whaley <michealwhaley42@gmail.com>
2024-02-15 13:22:14 -0600

    main testing code

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,11 +1,18 @@
 package test;
 
+import test.MediaProduct.Genre;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
 		StockManagerSingleton joe = new StockManagerSingleton("files/inventory.csv");
 		joe.intializeStock();
+		MediaProduct heat = new MediaProduct("Rainbow Goblins",24.99,1981,Genre.ELECTRONIC);
+		joe.addItem(heat);
+		System.out.println(heat.toString());
+		joe.updateItemPrice(heat, 5);
+		System.out.println(heat.toString());
 	}
 
 }
